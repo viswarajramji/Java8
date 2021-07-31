1be624bb84e10bf0c600e773737a9ac63854f83b
viswarajramji <viswarajramji93@gmail.com>
2021-07-31 15:29:35 +0400

    Debugging Stream operations

--- src/streams/DebuggingStreamOperations.java
+++ src/streams/DebuggingStreamOperations.java
@@ -1,5 +1,36 @@
 package streams;
 
-public class DebuggingStreamOperations {
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
+import data.Student;
+import data.StudentDB;
+
+public class DebuggingStreamOperations {
+	public static void main(String args[]) {
+		Predicate<Student> stuPre = (Student s) -> {
+			return s.getMark1() > 90 && s.getMark2() > 90;
+		};
+		Consumer<Student> consumer1=(Student s)->{
+			System.out.println("Consumer1 test");
+			System.out.println(s.getName());
+		};
+		
+		Consumer<Student> consumer2=(Student s)->{
+			System.out.println("Consumer2 test");
+			System.out.println(s.getName());
+		};
+		List<Student> listStd = StudentDB.getAllStudents();
+		List<Student> res=listStd.stream()
+				.peek(consumer1)
+				.filter(stuPre)
+				.peek(consumer2)
+				.collect(Collectors.toList());
+		System.out.println("Priting the result");
+		res.forEach((Student s) -> {
+			System.out.println(s.getName());
+		});
+	}
 }
