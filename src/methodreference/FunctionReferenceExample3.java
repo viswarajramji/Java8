09e49576fa6a6fc78593b11eef9e92c06bdf2d25
viswarajramji <viswarajramji93@gmail.com>
2021-08-01 01:20:10 +0400

    Function Reference Example

--- src/methodreference/FunctionReferenceExample3.java
+++ src/methodreference/FunctionReferenceExample3.java
@@ -3,29 +3,32 @@ package methodreference;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
-class Test{
-	public static String  getName() {
-		return "UserName";
-	}
-	
-	public static boolean predicate(String s) {
-		return s.isEmpty();
-	}
-}
+class Person {
 
+	public boolean isVote(Integer age) {
+		return age >= 18;
+	}
+
+}
 
 public class FunctionReferenceExample3 {
 	public static void main(String args[]) {
-	
+
 //		Supplier<String> s = ()-> Test.getName();
 //		Supplier<String> s1 = Test::getName;
-		
-		Predicate<String> s2 = (s)-> s.isEmpty();
-		System.out.println(s2.test(""));
-		
-		
-		Predicate<String> s3 = Test::predicate;
-		System.out.println(s3.test(""));
-		
+
+//		Predicate<String> s2 = (s) -> s.isEmpty();
+//		System.out.println(s2.test(""));
+//
+//		Predicate<String> s3 = Test::predicate;
+//		System.out.println(s3.test(""));
+		Person p=new Person();
+		Predicate<Integer> s1= p::isVote;
+		if(s1.test(18)) {
+			System.out.println("true");
+		}else {
+			System.out.println("false");
+		}
+
 	}
 }
