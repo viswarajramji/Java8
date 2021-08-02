bec999eeb63e46577f3135a1110e07b868e7f78f
viswarajramji <viswarajramji93@gmail.com>
2021-08-02 20:26:39 +0400

    StreamExamples

--- src/streams/StreamFunctionExamples.java
+++ src/streams/StreamFunctionExamples.java
@@ -2,6 +2,7 @@ package streams;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import data.Student;
@@ -18,15 +19,30 @@ public class StreamFunctionExamples {
 		sort();
 		customSort();
 		filter();
-		reduce();	
+		reduce();
+		reduceComp();
 	}
+	
+	public static void reduceComp() {
+		Optional<Student> maxScore=students.stream().reduce((s1,s2)->{
+			if(s1.getGpa()>s2.getGpa()) {
+				return s1;
+			}else {
+				return s2;
+			}
+		});
+		if(maxScore.isPresent()) {
+			System.out.println(maxScore.get().getName()+"__"+maxScore.get().getGpa());
+		}
+			
+	}
+	
 	public static void reduce() {
 		int totalMarks1=students.stream().map(Student::getMark1).reduce(0, (a,b)->a+b);
 		int totalMarks2=students.stream().map(Student::getMark2).reduce(0, (a,b)->a+b);
-		System.out.println(totalMarks1+" "+totalMarks2);
-		
-		
+		System.out.println(totalMarks1+" "+totalMarks2);		
 	}
+	
 	public static void filter() {
 		//activities containing swimming.
 		List<Student> resultList=students.stream().filter(s->s.getActivity().contains("swimming")).collect(Collectors.toList());
