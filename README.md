1ff952f433aded51867095abf3bab9171ce2c806
viswarajramji <viswarajramji93@gmail.com>
2021-08-02 14:44:21 +0400

    StreamsExamples

--- README.md
+++ README.md
@@ -4,11 +4,22 @@ This repository contain all the Java 8 related concepts and examples.
 
 ##### Notes:
 
+##### Lambda:
+
 * The lambda variable name and the local variable names cannot be same.
 * The local variables cannot be modified inside lambda expressions.
 * No restrictions for instance variables.
 * A local variable used inside a lambda expression is termed as effectively final. [Refer](https://github.com/viswarajramji/Java8/blob/master/src/lambda/LambdaLocalVariables.java) 
 * In General until Java 7, Any variables used inside anonymous function must be declared as final. 
+
+
+
+##### Streams:
+
+* Collect method is the one that invokes the streams.
+* Streams are lazy loading.
+
+
 
 
 
--- src/streams/StreamsExample2.java
+++ src/streams/StreamsExample2.java
@@ -2,16 +2,18 @@ package streams;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import data.Student;
 import data.StudentDB;
 
-public class StreamsExample1 {
+public class StreamsExample2 {
 	public static void main(String args[]) {
+		Predicate<Student> gpa = (Student s) -> s.getGpa() >= 4.0;
 		List<Student> listStudents = StudentDB.getAllStudents();
-		Map<String, List<String>> map = listStudents.stream().collect(Collectors.toMap(Student::getName, Student::getActivity));
+		Map<String, List<String>> map = listStudents.stream().filter(gpa).collect(Collectors.toMap(Student::getName, Student::getActivity));
 		System.out.println(map.toString());
-		
+
 	}
 }
