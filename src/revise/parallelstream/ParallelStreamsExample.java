db8dbd5c3870912235b72b2b20da13915306c91c
viswarajramji <viswarajramji93@gmail.com>
2021-08-06 22:37:08 +0400

    Parallel Stream

--- src/revise/parallelstream/ParallelStreamsExample.java
+++ src/revise/parallelstream/ParallelStreamsExample.java
@@ -1,17 +1,29 @@
 package revise.parallelstream;
 
+import java.util.function.Supplier;
 import java.util.stream.IntStream;
 
 public class ParallelStreamsExample {
 	public static void main(String args[]) {
-		basicExample();
+		performance(ParallelStreamsExample::Stream, 1000);
+		performance(ParallelStreamsExample::parallelStream, 1000);
 	}
 
-	public static void basicExample() {
-		int sumValue = IntStream.rangeClosed(1, 100).sum();
-		System.out.println(sumValue);
-
-		sumValue = IntStream.rangeClosed(1, 100).parallel().sum();
-		System.out.println(sumValue);
+	public static void performance(Supplier<Integer> supplier, int num) {
+		Long start = System.currentTimeMillis();
+		for (int i = 0; i < num; i++) {
+			supplier.get();
+		}
+		Long end = System.currentTimeMillis();
+		System.out.println(end - start);
 	}
+
+	public static Integer Stream() {
+		return IntStream.rangeClosed(1, 100).sum();
+	}
+
+	public static Integer parallelStream() {
+		return IntStream.rangeClosed(1, 100).sum();
+	}
+
 }
