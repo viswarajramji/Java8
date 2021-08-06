d9276de5b0c2ea636d9aa4c45c3c3707bb8c453e
viswarajramji <viswarajramji93@gmail.com>
2021-08-07 01:04:30 +0400

    Min and max By

--- src/revise/terminaloperation/TerminalExample1.java
+++ src/revise/terminaloperation/TerminalExample1.java
@@ -1,7 +1,9 @@
 package revise.terminaloperation;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import data.Student;
@@ -15,15 +17,38 @@ public class TerminalExample1 {
 		counting();
 		System.out.println("***Mapping***");
 		mapping();
+		System.out.println("***MinBy***");
+		minBy();
+		System.out.println("***MaxBy***");
+		MaxBy();
+	}
+
+	public static void MaxBy() {
+		List<Student> students = StudentDB.getAllStudents();
+		Optional<Student> value = students.stream().collect(Collectors.maxBy(Comparator.comparing(Student::getName)));
+		if (value.isPresent()) {
+			System.out.println(value.get().getName());
+			return;
+		}
+		System.out.println("Not found");
+
+	}
+
+	public static void minBy() {
+		List<Student> students = StudentDB.getAllStudents();
+		Optional<Student> value = students.stream().collect(Collectors.minBy(Comparator.comparing(Student::getName)));
+		if (value.isPresent()) {
+			System.out.println(value.get().getName());
+			return;
+		}
+		System.out.println("Not found");
+
 	}
 
 	public static void mapping() {
 		List<Student> students = StudentDB.getAllStudents();
-		String str= 
-				students
-				.stream()
-				.collect(Collectors.mapping(Student::getName, Collectors.toSet()))
-				.stream().collect(Collectors.joining(" "));
+		String str = students.stream().collect(Collectors.mapping(Student::getName, Collectors.toSet())).stream()
+				.collect(Collectors.joining(" "));
 		System.out.println(str);
 
 	}
