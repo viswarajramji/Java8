6affe9cdc5af13d12e54530918168e9db2405816
viswarajramji <viswarajramji93@gmail.com>
2021-08-09 23:49:23 +0400

    Terminal Example

---
+++ src/revise/terminaloperation/TerminalExample2.java
@@ -0,0 +1,18 @@
+package revise.terminaloperation;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TerminalExample2 {
+	public static void main(String args[]) {
+		System.out.println("Joining method");
+		joining();
+	}
+
+	public static void joining() {
+		List<String> values = Arrays.asList("1", "2", "3", "4");
+		String join = values.stream().collect(Collectors.joining());
+		System.out.println(join);
+	}
+}
