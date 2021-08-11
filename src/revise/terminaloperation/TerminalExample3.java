c32bf48d519eca2c2fe86c4d7b49d890369df42b
viswarajramji <viswarajramji93@gmail.com>
2021-08-11 21:26:33 +0400

    Example3

--- src/revise/terminaloperation/TerminalExample3.java
+++ src/revise/terminaloperation/TerminalExample3.java
@@ -10,6 +10,21 @@ import data.StudentDB;
 public class TerminalExample3 {
 	public static void main(String args[]) {
 		groupByExample1();
+		groupByExample2();
+	}
+
+	public static void groupByExample2() {
+		List<Student> student = StudentDB.getAllStudents();
+		Map<String, List<Student>> values = student.stream().collect(Collectors.groupingBy((Student s) -> {
+			if ("male".equals(s.getGender())) {
+				return "MEN";
+			}
+			return "WOMEN";
+		}));
+		values.forEach((gender, listOfStudent) -> {
+			System.out.println("Gender : " + gender);
+			listOfStudent.stream().map(Student::getName).forEach(System.out::println);
+		});
 	}
 
 	public static void groupByExample1() {
