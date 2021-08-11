4a47fee1fcfb884347457dcf8b9fd21335899f9d
viswarajramji <viswarajramji93@gmail.com>
2021-08-12 01:46:12 +0400

    reduce method

--- src/revise/streams/StreamExample2.java
+++ src/revise/streams/StreamExample2.java
@@ -1,13 +1,44 @@
 package revise.streams;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.OptionalInt;
 import java.util.stream.IntStream;
+
+import data.Student;
+import data.StudentDB;
 
 public class StreamExample2 {
 	public static void main(String args[]) {
 		example1();
 		example2();
+		example3();
+		example4();
+	}
+
+	public static void example4() {
+		// find max of the student
+		List<Student> listOfStudents = StudentDB.getAllStudents();
+		Optional<Student> maxStudent = listOfStudents.stream().reduce((s1,s2)->{
+			if(s1.getGpa() > s2.getGpa()) {
+				return s1;
+			}
+			return s2;
+		});
+		maxStudent.ifPresent(s->{
+			System.out.println(s.getName() +" "+s.getGpa() );
+		});
+	}
+
+	public static void example3() {
+		OptionalInt maxValue = IntStream.rangeClosed(1, 100).reduce((a, b) -> {
+			if (a > b) {
+				return a;
+			}
+			return b;
+		});
+		maxValue.ifPresent(System.out::println);
+
 	}
 
 	public static void example1() {
