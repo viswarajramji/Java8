2b6ef09d4ecf738135e37a24a524cdba2d63457e
viswarajramji <viswarajramji93@gmail.com>
2021-08-12 23:54:22 +0400

    predicate and modication

--- src/revise/lambdas/LambdasExamples1.java
+++ src/revise/lambdas/LambdasExamples1.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.UUID;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -12,21 +13,51 @@ public class LambdasExamples1 {
 		example1(10);
 		example2();
 		example3();
+		example4();
+
+	}
+
+	public static void example4() {
+		Predicate<Integer> agePredicate = ((Integer i) -> {
+			if (i < 18) {
+				return true;
+			}
+			return false;
+		});
+
+		Predicate<Integer> isMale = ((Integer i) -> {
+			if (i > 5) {
+				return true;
+			}
+			return false;
+		});
+
+		System.out.println(agePredicate.and(isMale).test(10));
 	}
 
 	public static void example3() {
-		BiConsumer<String, String> biConsumer = ((x, y) -> {
+		BiConsumer<String, String> biConsumer1 = ((x, y) -> {
 			System.out.println("x : " + x + "y : " + y);
 		});
-		biConsumer.accept("test1", "test2");
+
+		BiConsumer<String, String> biConsumer2 = ((x, y) -> {
+			System.out.println("x : " + x + "y : " + y);
+		});
+
+		biConsumer1.andThen(biConsumer2).accept("test1", "test2");
 	}
 
 	public static void example2() {
-		Consumer<String> consumer = (s -> {
+		Consumer<String> consumer1 = (s -> {
 			System.out.println(s);
 		});
 
-		consumer.accept("testUser");
+		Consumer<String> consumer2 = (s -> {
+			System.out.println(s);
+		});
+
+		consumer1.andThen(consumer2).accept("test");
+
 	}
 
 	public static void example1(int limit) {
