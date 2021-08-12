3b40cc03b30a5a5808410740a486ca1d54e1d970
viswarajramji <viswarajramji93@gmail.com>
2021-08-12 23:04:12 +0400

    Adhoc changes

---
+++ src/revise/adhoc/EffectivelyFinal.java
@@ -0,0 +1,13 @@
+package revise.adhoc;
+
+public class EffectivelyFinal {
+	public static void main(String args[]) {
+//		Error code changes
+//		int a = 10;
+//		Stream<String> streamList = Stream.of("a", "b", "c");
+//		streamList.forEach(s -> {
+//			System.out.println(a);
+//		});
+//		a=10;
+	}
+}
