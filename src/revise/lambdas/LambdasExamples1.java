381073eda665bf5a70a61a9b7d79d87c945a1ad9
viswarajramji <viswarajramji93@gmail.com>
2021-08-12 23:40:26 +0400

    Lambdas

---
+++ src/revise/lambdas/LambdasExamples1.java
@@ -0,0 +1,39 @@
+package revise.lambdas;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class LambdasExamples1 {
+	public static void main(String args[]) {
+		example1(10);
+		example2();
+	
+	}
+	
+	
+
+	public static void example2() {
+		Consumer<String> consumer = (s -> {
+			System.out.println(s);
+		});
+
+		consumer.accept("testUser");
+	}
+
+	public static void example1(int limit) {
+		List<String> streamList = Stream.generate(UUID::randomUUID).limit(limit).map(UUID::toString)
+				.collect(Collectors.toList());
+		streamList.forEach((String s) -> {
+			System.out.println(s);
+		});
+
+		streamList.forEach(s -> {
+			System.out.println(s);
+		});
+
+		streamList.forEach(System.out::println);
+	}
+}
